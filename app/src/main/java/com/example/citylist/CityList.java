45c306ace350b25cfe9fbef7758d507e9688587f
kazol196295 <kazol196295@gmail.com>
2023-03-28 19:44:26 +0600

    javadoc created

--- app/src/main/java/com/example/citylist/CityList.java
+++ app/src/main/java/com/example/citylist/CityList.java
@@ -2,6 +2,7 @@ package com.example.citylist;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -23,13 +24,48 @@ public class CityList {
     }
 
     /**
-     * This returns a sorted list of cities
+     * This returns a sorted list of cities or province
      * @return
-     *      Return the sorted list of cities
+     *      Return the sorted list of cities if x=1 otherwise return sorted list of  province
      */
-    public List<City> getCities() {
+    public List<City> getCities(int x) {
         List<City> cityList = cities;
-        Collections.sort(cityList);
+        if(x==1){
+            Collections.sort(cityList);
+        }
+        else{
+            Collections.sort(cityList, new Comparator<City>() {
+                @Override
+                public int compare(City city, City t1) {
+                        return city.getProvinceName().compareTo(t1.getProvinceName());
+                }
+            });
+        }
+
         return cityList;
     }
+
+    /**
+     * This function delete City from cityList
+     * @param city
+     *          This will be deleted from cityList
+     */
+    public void delete(City city){
+        if(!cities.contains(city)){
+            throw new IllegalArgumentException();
+        }
+        else {
+            cities.remove(city);
+        }
+    }
+
+    /**
+     * @return
+     *      This return number of city
+     */
+    public int count()
+    {
+        return cities.size();
+    }
+
 }
